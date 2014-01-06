fbd4d46abb8f8a790f52e3241dde52d370f0a387
Jesse Squires <jesse.d.squires@gmail.com>
2014-01-05 22:05:03 -0800

    add/remove play icon

--- js/freedomfarts.js
+++ js/freedomfarts.js
@@ -1,13 +1,36 @@
+var CURRENTLY_PLAYING;
 
 function did_click_fart_button (btn) {
+	$('.glyphicon', btn).remove();
+
 	var title = $(btn).text().toLowerCase();
+
+	console.log("Playing " + title + "...");
 	
+	var player = document.getElementById('audioplayer');
+
+	if (!player.paused && CURRENTLY_PLAYING === title) {
+		player.pause();
+		player.currentTime = 0;
+		$('.glyphicon', btn).remove();
+		return;
+	}
+
 	addAudioSource('#src_wav', title, 'wav');
 	addAudioSource('#src_mp3', title, 'mp3');
 	addAudioSource('#src_ogg', title, 'ogg');
 
-	document.getElementById('audioplayer').load();
-	document.getElementById('audioplayer').play();
+	player.load();
+	player.play();
+	CURRENTLY_PLAYING = title;
+
+	$(btn).prepend('<span class="glyphicon glyphicon-play">&nbsp;</span>');
+
+	$(player).on('ended', function() {
+		console.log("Finished " + title + "!");
+		$('.glyphicon', btn).remove();
+		$(player).off();
+	});
 }
 
 function addAudioSource(srcid, title, ext) {
