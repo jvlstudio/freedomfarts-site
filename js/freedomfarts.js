dc1cbba9d39b1eb9bbb3cb82a59cda739377522d
Jesse Squires <jesse.d.squires@gmail.com>
2014-01-05 20:48:46 -0800

    sound files. first attempt at audio.

--- js/freedomfarts.js
+++ js/freedomfarts.js
@@ -1,8 +1,17 @@
 
 function did_click_fart_button (btn) {
 	var title = $(btn).text().toLowerCase();
-
+	
 	console.log("Playing " + title + "...");
 
-	
+	addAudioSource('#src_wav', title, 'wav');
+	addAudioSource('#src_mp3', title, 'mp3');
+	addAudioSource('#src_ogg', title, 'ogg');
+
+	document.getElementById('audioplayer').play();
+}
+
+function addAudioSource(srcid, title, ext) {
+	var src = $(srcid);
+	src.attr('src', '/audio/' + title + '.' + ext);
 }
